7ead4a5906655068bdf87ec3766e568627988e6c
ctrindadedev <caiomedtrindade@gmail.com>
2025-09-04 03:00:50 -0300

    add JSDocs format

--- src/currency/currency.service.ts
+++ src/currency/currency.service.ts
@@ -1,28 +1,58 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, NotFoundException } from "@nestjs/common";
 
+/**
+ * @class CurrencyService
+ * @description Fornece funcionalidades para conversão e listagem de moedas
+ */
 @Injectable()
 export class CurrencyService {
+  /**
+   * @private
+   * @description Lista de moedas e suas taxas de câmbio em relação a uma moeda base
+   */
   private currencies = [
     { code: "USD", rate: 1.0 },
     { code: "EUR", rate: 0.85 },
     { code: "BRL", rate: 5.25 },
   ];
 
+  /**
+   * Retorna uma lista com os códigos de todas as moedas disponíveis para conversão
+   * @returns {string[]} Array de strings contendo os códigos das moedas
+   */
   getAvailableCurrencies(): string[] {
     return this.currencies.map((currency) => currency.code);
   }
 
+  /**
+   * Realiza a conversão de um valor entre duas moedas com base nas taxas de câmbio internas
+   *
+   * @param {number} amount - O valor a ser convertido. Deve ser > 0
+   * @param {string} from - O código da moeda de origem
+   * @param {string} to - O código da moeda de destino
+   * @returns {number} - O valor convertido para a moeda de destino
+   * @throws {NotFoundException} Lança uma exceção se o código da moeda de origem ou de destino não for encontrado
+   * * @example
+   * // Exemplo de uso:
+   * const convertedValue = currencyService.convertCurrency(100, 'BRL', 'USD');
+   */
   convertCurrency(amount: number, from: string, to: string): number {
-    const fromRate = this.currencies.find(
+    const fromCurrency = this.currencies.find(
       (currency) => currency.code === from
     )?.rate;
-    const toRate = this.currencies.find(
+
+    const toCurrency = this.currencies.find(
       (currency) => currency.code === to
     )?.rate;
-    if (!fromRate || !toRate) {
-      throw new Error("Currency not found");
+
+    if (!fromCurrency || !toCurrency) {
+      throw new NotFoundException(
+        `Currency code not found. Please use one of the following: ${this.getAvailableCurrencies().join(", ")}`
+      );
     }
-    const convertedAmount = (amount / fromRate) * toRate;
-    return convertedAmount;
+
+    const convertedAmount = (amount / fromCurrency) * toCurrency;
+
+    return parseFloat(convertedAmount.toFixed(2));
   }
 }
